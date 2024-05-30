b77ef90d6615a3ecf5969815303de43bad4f1fe0
BudzioT <Budzio.zsm1@gmail.com>
2024-05-30 16:26:50 +0200

    Program is stuck in the loop, which is desired, disassembler was added for testing

--- main.cpp
+++ main.cpp
@@ -2,11 +2,16 @@
 #include <cstdio>
 #include <fstream>
 #include "Emulator8080.h"
+#include "Disassembler8080.h"
 
 int main() {
+    setvbuf(stdout, NULL, _IONBF, 0);
+    /*
     std::cout << "File path: " << std::endl;
     std::string path;
     std::cin >> path;
+    */
+    std::string path = "D:\\Pobrane\\Github_projects\\Intel8080-ConsoleEmulator\\ROMS\\SpaceInvaders\\invaders.concatenated";
 
     FILE* file = fopen(path.c_str(), "rb");
     if (!file) {
@@ -15,15 +20,20 @@ int main() {
     }
 
     fseek(file, 0, SEEK_END);
-    int size = ftell(file);
+    unsigned int size = ftell(file);
     auto* buffer = new unsigned char[size];
     fseek(file, 0, SEEK_SET);
 
     fread(buffer, size, 1, file);
     fclose(file);
 
+    int pc = 0;
+    int line = 0;
+
     Emulator8080 emulator(buffer);
     while(emulator.ProgramCounter() < size) {
+        printf("%d: ", ++line);
+        disassembler(buffer, emulator.ProgramCounter());
         emulator.Emulate();
     }
 
